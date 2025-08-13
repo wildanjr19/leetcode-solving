1a20997e7a2bca88b7852c0dde104ba8026c6675
wildanjr19 <wildansekolah11@gmail.com>
2025-08-13 11:44:11 +0700

    pandas;595

---
+++ 595/595.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def big_countries(world: pd.DataFrame) -> pd.DataFrame:
+    result = world[(world['area'] >= 3000000) | (world['population'] >= 25000000)]
+
+    return result[['name', 'population', 'area']]
